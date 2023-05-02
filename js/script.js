77c0c2fb414fa9530c2a55a79b63238dba03abf7
zhakanuly <106392331+zhakanuly@users.noreply.github.com>
2023-05-02 19:53:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,101 @@
+//Budget
+const budgetInput = document.querySelector(".budget_input");
+const budgetButton = document.querySelector("#btn_budget");
+
+//Expenses
+const expensesNameInput = document.querySelector(".purchase_name");
+const expensesPriceInput = document.querySelector(".purchase_price");
+const expensesButton = document.querySelector("#btn_purchase");
+
+//View
+const budgetView = document.querySelector(".budget_view");
+const expensesView = document.querySelector(".expenses_view");
+const balanceView = document.querySelector(".balance_view");
+
+//Шығындар тізім
+const expensesListView = document.querySelector(".exp_list_view")
+
+
+// Бюджет сақтау
+budgetButton.addEventListener("click", () => {
+    budgetView.innerHTML = budgetInput.value;
+    budgetInput.value = 0;
+    calcBalance();
+});
+
+
+//Шығын сақтау, бюджеттен азайту
+expensesButton.addEventListener("click", () => {
+    //Аты мен бағаны айнымалыға жазып алу
+    const title = expensesNameInput.value;
+    const price = expensesPriceInput.value;
+
+    const expensesListElements = document.createElement("div");
+    expensesListElements.setAttribute("class", "exp_list_elements")
+
+    const expListTitle = document.createElement("div");
+    expListTitle.setAttribute("class", "exp_list_title");
+    expListTitle.innerHTML = title;
+
+    const expListPrice = document.createElement("div");
+    expListPrice.setAttribute("class", "exp_list_price");
+    expListPrice.innerHTML = price;
+
+    const expRemoveBtn = document.createElement("button");
+    expRemoveBtn.innerHTML = "Өшіру";
+    expRemoveBtn.setAttribute("class", "exp_remove_btn");
+    expRemoveBtn.addEventListener("click", (e) => {
+        // console.log('333',e.target)
+        e.target.parentElement.remove(); 
+        const removeValue = Number(expListPrice.innerHTML);
+        expensesView.innerHTML = Number(expensesView.innerHTML) - removeValue;
+        balanceView.innerHTML = Number(balanceView.innerHTML) + removeValue;
+});
+
+    const expChangeBtn = document.createElement("button");
+    expChangeBtn.innerHTML = "Өзгерту";
+    expChangeBtn.setAttribute("class", "exp_change_btn")
+    expChangeBtn.addEventListener("click", (e) => {
+        const aaa = e.target.parentElement;
+        expensesNameInput.value = expListTitle.innerHTML;
+        expensesPriceInput.value = expListPrice.innerHTML;
+    });
+
+    expensesListElements.appendChild(expListTitle);
+    expensesListElements.appendChild(expListPrice);
+    expensesListElements.appendChild(expRemoveBtn);
+    expensesListElements.appendChild(expChangeBtn);
+    expensesListView.appendChild(expensesListElements);
+
+    expListTitle.innerHTML = title;
+    expListPrice.innerHTML = price;
+    expensesNameInput.value = " ";
+    expensesPriceInput.value = " ";
+
+
+    calcExpenses();
+    calcBalance();
+});
+
+function calcExpenses() {
+    let total = 0;
+    const all = document.querySelectorAll(".exp_list_elements");
+
+    all.forEach((x,index) => {
+        console.log(x);
+        const price = x.querySelector(".exp_list_price");
+        total += Number(price.innerHTML);
+    });
+    expensesView.innerHTML = total;
+};
+
+function removeExpenses() {
+    
+};
+
+
+
+//Баланс есептеу функция
+function calcBalance() {
+    balanceView.innerHTML = budgetView.innerHTML - expensesView.innerHTML;
+};
