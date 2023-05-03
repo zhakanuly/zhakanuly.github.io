ea3a3cd29c1b56b4d8b6102a775ca4b14d4e90ea
zhakanuly <106392331+zhakanuly@users.noreply.github.com>
2023-05-03 13:45:25 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const budgetButton = document.querySelector("#btn_budget");
 const expensesNameInput = document.querySelector(".purchase_name");
 const expensesPriceInput = document.querySelector(".purchase_price");
 const expensesButton = document.querySelector("#btn_purchase");
+var flag = false;
 
 //View
 const budgetView = document.querySelector(".budget_view");
@@ -26,62 +27,14 @@ budgetButton.addEventListener("click", () => {
 
 //Шығын сақтау, бюджеттен азайту
 expensesButton.addEventListener("click", () => {
-    //Аты мен бағаны айнымалыға жазып алу
-    const title = expensesNameInput.value;
-    const price = expensesPriceInput.value;
-
-    const expensesListElements = document.createElement("div");
-    expensesListElements.setAttribute("class", "exp_list_elements")
-
-    const expListTitle = document.createElement("div");
-    expListTitle.setAttribute("class", "exp_list_title");
-    expListTitle.innerHTML = title;
-
-    const expListPrice = document.createElement("div");
-    expListPrice.setAttribute("class", "exp_list_price");
-    expListPrice.innerHTML = price;
-
-    const expRemoveBtn = document.createElement("button");
-    expRemoveBtn.innerHTML = "Өшіру";
-    expRemoveBtn.setAttribute("class", "exp_remove_btn");
-    expRemoveBtn.addEventListener("click", (e) => {
-        // console.log('333',e.target)
-        e.target.parentElement.remove(); 
-        const removeValue = Number(expListPrice.innerHTML);
-        expensesView.innerHTML = Number(expensesView.innerHTML) - removeValue;
-        balanceView.innerHTML = Number(balanceView.innerHTML) + removeValue;
+    expBtnRegim1();
 });
-
-    const expChangeBtn = document.createElement("button");
-    expChangeBtn.innerHTML = "Өзгерту";
-    expChangeBtn.setAttribute("class", "exp_change_btn")
-    expChangeBtn.addEventListener("click", (e) => {
-        const aaa = e.target.parentElement;
-        expensesNameInput.value = expListTitle.innerHTML;
-        expensesPriceInput.value = expListPrice.innerHTML;
-    });
-
-    expensesListElements.appendChild(expListTitle);
-    expensesListElements.appendChild(expListPrice);
-    expensesListElements.appendChild(expRemoveBtn);
-    expensesListElements.appendChild(expChangeBtn);
-    expensesListView.appendChild(expensesListElements);
-
-    expListTitle.innerHTML = title;
-    expListPrice.innerHTML = price;
-    expensesNameInput.value = " ";
-    expensesPriceInput.value = " ";
-
-
-    calcExpenses();
-    calcBalance();
-});
-
+//Шығын есептеу
 function calcExpenses() {
     let total = 0;
     const all = document.querySelectorAll(".exp_list_elements");
 
-    all.forEach((x,index) => {
+    all.forEach((x) => {
         console.log(x);
         const price = x.querySelector(".exp_list_price");
         total += Number(price.innerHTML);
@@ -89,13 +42,64 @@ function calcExpenses() {
     expensesView.innerHTML = total;
 };
 
-function removeExpenses() {
-    
-};
-
-
-
 //Баланс есептеу функция
 function calcBalance() {
     balanceView.innerHTML = budgetView.innerHTML - expensesView.innerHTML;
+};
+
+function expBtnRegim1() {
+    if(flag == false) {
+        const title = expensesNameInput.value;
+        const price = expensesPriceInput.value;
+
+        const expensesListElements = document.createElement("div");
+        expensesListElements.setAttribute("class", "exp_list_elements")
+
+        const expListTitle = document.createElement("div");
+        expListTitle.setAttribute("class", "exp_list_title");
+        expListTitle.innerHTML = title;
+
+        const expListPrice = document.createElement("div");
+        expListPrice.setAttribute("class", "exp_list_price");
+        expListPrice.innerHTML = price;
+
+        const expRemoveBtn = document.createElement("button");
+        expRemoveBtn.innerHTML = "Өшіру";
+        expRemoveBtn.setAttribute("class", "exp_remove_btn");
+        expRemoveBtn.addEventListener("click", (e) => {
+            // console.log('333',e.target)
+            e.target.parentElement.remove(); 
+            const removeValue = Number(expListPrice.innerHTML);
+            expensesView.innerHTML = Number(expensesView.innerHTML) - removeValue;
+            balanceView.innerHTML = Number(balanceView.innerHTML) + removeValue;
+    });
+
+        const expChangeBtn = document.createElement("button");
+        expChangeBtn.innerHTML = "Өзгерту";
+        expChangeBtn.setAttribute("class", "exp_change_btn")
+        expChangeBtn.addEventListener("click", (e) => {
+            const aaa = e.target.parentElement;
+            console.log(aaa);
+            expensesNameInput.value = expListTitle.innerHTML;
+            expensesPriceInput.value = expListPrice.innerHTML;
+            flag = true;
+        });
+
+        expensesListElements.appendChild(expListTitle);
+        expensesListElements.appendChild(expListPrice);
+        expensesListElements.appendChild(expRemoveBtn);
+        expensesListElements.appendChild(expChangeBtn);
+        expensesListView.appendChild(expensesListElements);
+
+        expListTitle.innerHTML = title;
+        expListPrice.innerHTML = price;
+        expensesNameInput.value = " ";
+        expensesPriceInput.value = " ";
+
+
+        calcExpenses();
+        calcBalance();
+    } else {
+        flag = false;
+    }
 };
